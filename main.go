03e981ac268ea7f14e2a9a65bab8d0c40ffdf73f
Christopher Sampson <cjsampson41@gmail.com>
2025-08-13 22:32:27 -0600

    additional comments on goals and learning path' g

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 
 // create a server with chromedp and send data to it
 // 1. create a client/server relationhip and have a
+// 2. need to brush up on machine learning and calculus
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 var mode = flag.String("mode", "client", "host or client")
